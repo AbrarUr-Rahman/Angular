f48aa6b8f3698e7d2bdc63bc0354c8cee5324daa
Md Abrar Ur Rahman <ishmam000@gmail.com>
2024-10-09 19:08:18 +0600

    commit

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,40 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
-import { HeaderComponent } from "./header/header.component";
+// import { Component, OnInit } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { HomeComponent } from "./home/home.component";
 
-@Component({
-  selector: 'app-root',
-  standalone: true, //!Will Import anywhere
-  imports: [RouterOutlet, HomeComponent, RouterLink, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
-export class AppComponent 
-// implements OnInit
-{
-    // title="Code With Abrar";
+// @Component({
+//   selector: 'app-root',
+//   standalone: true, //!Will Import anywhere
+//   imports: [RouterOutlet, HomeComponent],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.scss'
+// })
+// export class AppComponent implements OnInit{
+//     // title="Code With Abrar";
 
-    // //#Constructor
-    // //*Loads First on load
-    // constructor(){
-    //   console.log('constructor')
-    // }
+//     // //#Constructor
+//     // //*Loads First on load
+//     // constructor(){
+//     //   console.log('constructor')
+//     // }
 
-    // //!Life Cycle
-    // //*Loads after constructor
-    // ngOnInit(){
-    //   console.log('ngoninit')
-    //   // this.changeTitle();
-    // }
+//     // //!Life Cycle
+//     // //*Loads after constructor
+//     // ngOnInit(){
+//     //   console.log('ngoninit')
+//     //   // this.changeTitle();
+//     // }
 
-    // changeTitle(){
-    //   this.title="Coding Techniques"
-    // }
+//     // changeTitle(){
+//     //   this.title="Coding Techniques"
+//     // }
 
 
     //!------------New Stuff 1-----------------//
-  
+    import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+// import { HeaderComponent } from './main-app/header/header.component';
+// import { FooterComponent } from './main-app/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, 
+    // HeaderComponent,
+    
+    // FooterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+
+}
     //!------------New Stuff 1-----------------//
 
 }
